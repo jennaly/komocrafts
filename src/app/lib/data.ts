43a2339a37993f8f6c3d9bcd9292cacbf2c1e8f9
jennaly <106183040+jennaly@users.noreply.github.com>
2024-04-11 15:22:47 -0700

    feat: set up functions to fetch product data from shopify

---
+++ src/app/lib/data.ts
@@ -0,0 +1,292 @@
+import { gql } from "graphql-request";
+import { graphQLClient } from "./client";
+import { ProductsData, ShopifyProductData, CategoryData } from "./definitions";
+
+export const getAllPhysicalProducts = async (): Promise<ProductsData> => {
+  const getAllPhysicalProductsQuery = gql`
+    query PhysicalProductsQuery {
+      products(first: 250, query: "tag_not:Workshops") {
+        nodes {
+          title
+          handle
+          id
+          tags
+          description
+          featuredImage {
+            id
+            url
+            width
+            height
+            altText
+          }
+          images(first: 3) {
+            edges {
+              node {
+                id
+                url
+                width
+                height
+                altText
+              }
+            }
+          }
+          variants(first: 100) {
+            edges {
+              node {
+                title
+                id
+                image {
+                  id
+                  url
+                  width
+                  height
+                  altText
+                }
+              }
+            }
+          }
+          priceRange {
+            minVariantPrice {
+              amount
+              currencyCode
+            }
+            maxVariantPrice {
+              amount
+              currencyCode
+            }
+          }
+          compareAtPriceRange {
+            minVariantPrice {
+              amount
+              currencyCode
+            }
+            maxVariantPrice {
+              amount
+              currencyCode
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    return await graphQLClient.request(getAllPhysicalProductsQuery);
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
+
+export const getSingleProduct = async (
+  handle: string
+): Promise<ShopifyProductData> => {
+  const getSingleProductQuery = gql`
+    query SingleProductQuery($handle: String!) {
+      product(handle: $handle) {
+        title
+        handle
+        id
+        description
+        tags
+        featuredImage {
+          id
+          url
+          width
+          height
+          altText
+        }
+        images(first: 20) {
+          edges {
+            node {
+              id
+              url
+              width
+              height
+              altText
+            }
+          }
+        }
+        variants(first: 100) {
+          edges {
+            node {
+              title
+              id
+              image {
+                id
+                url
+                width
+                height
+                altText
+              }
+            }
+          }
+        }
+        priceRange {
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+          maxVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+        compareAtPriceRange {
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+          maxVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+      }
+    }
+  `;
+  const variables = {
+    handle: handle,
+  };
+
+  try {
+    return await graphQLClient.request(getSingleProductQuery, variables);
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
+
+export const getWorkshops = async (): Promise<CategoryData> => {
+  const getWorkshops = gql`
+    query WorkshopsCollection {
+      collection(handle: "workshops") {
+        handle
+        products(first: 250) {
+          edges {
+            node {
+              title
+              handle
+              id
+              tags
+              description
+              featuredImage {
+                id
+                url
+                width
+                height
+                altText
+              }
+              images(first: 3) {
+                edges {
+                  node {
+                    id
+                    url
+                    width
+                    height
+                    altText
+                  }
+                }
+              }
+              priceRange {
+                minVariantPrice {
+                  amount
+                  currencyCode
+                }
+                maxVariantPrice {
+                  amount
+                  currencyCode
+                }
+              }
+              compareAtPriceRange {
+                minVariantPrice {
+                  amount
+                  currencyCode
+                }
+                maxVariantPrice {
+                  amount
+                  currencyCode
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    return await graphQLClient.request(getWorkshops);
+  } catch (error) {
+    throw new Error(`
+    ${error}`);
+  }
+};
+
+export const getProductsByCategory = async (
+  categoryHandle: string
+): Promise<CategoryData> => {
+  const getProductsByCategoryQuery = gql`
+    query ProductsByCategory($handle: String) {
+      collection(handle: $handle) {
+        handle
+        products(first: 250) {
+          edges {
+            node {
+              title
+              handle
+              id
+              tags
+              description
+              featuredImage {
+                id
+                url
+                width
+                height
+                altText
+              }
+              images(first: 3) {
+                edges {
+                  node {
+                    id
+                    url
+                    width
+                    height
+                    altText
+                  }
+                }
+              }
+              priceRange {
+                minVariantPrice {
+                  amount
+                  currencyCode
+                }
+                maxVariantPrice {
+                  amount
+                  currencyCode
+                }
+              }
+              compareAtPriceRange {
+                minVariantPrice {
+                  amount
+                  currencyCode
+                }
+                maxVariantPrice {
+                  amount
+                  currencyCode
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+  const variables = {
+    handle: categoryHandle,
+  };
+
+  try {
+    return await graphQLClient.request(getProductsByCategoryQuery, variables);
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
