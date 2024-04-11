2d67647530507bd0a9e066421309a2536a16353f
jennaly <106183040+jennaly@users.noreply.github.com>
2024-04-11 16:24:10 -0700

    chore: import static assets (categories collage)

---
+++ src/app/lib/staticData.ts
@@ -0,0 +1,39 @@
+export const Categories = ["Coasters", "Wall Hangings"];
+export const categoriesCollage = [
+  {
+    title: "Coasters",
+    description: "From $16.00",
+    handle: "coasters",
+    backgroundImg: {
+      file: "shop-coasters.jpeg",
+      position: "bottom",
+    },
+  },
+  {
+    title: "Wall Hangings",
+    description: "From $18.00",
+    handle: "wall-hangings",
+    backgroundImg: {
+      file: "shop-wallhangings.jpg",
+      position: "bottom",
+    },
+  },
+  {
+    title: "Keychains",
+    description: "From $14.00",
+    handle: "keychains",
+    backgroundImg: {
+      file: "shop-wallhangings.jpg",
+      position: "center",
+    },
+  },
+  {
+    title: "Supplies",
+    description: "From $5.00",
+    handle: "supplies",
+    backgroundImg: {
+      file: "shop-supplies.jpg",
+      position: "center",
+    },
+  },
+];
