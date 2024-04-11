886cde770a895965f6d99ed6c6fa58573871fff5
jennaly <106183040+jennaly@users.noreply.github.com>
2024-04-11 15:23:56 -0700

    feat: define GraphQL response types for product requests

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,60 @@
+type ShopifyImage = {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+  altText: null;
+};
+
+type VariantPrice = {
+  minVariantPrice: {
+    amount: string;
+    currencyCode: string;
+  };
+  maxVariantPrice: {
+    amount: string;
+    currencyCode: string;
+  };
+};
+
+export type ShopifyProductData = {
+  title: string;
+  handle: string;
+  id: string;
+  description: string;
+  tags: string[];
+  featuredImage: ShopifyImage;
+  images: {
+    edges: {
+      node: ShopifyImage;
+    };
+  };
+  variants?: {
+    edges: {
+      node: {
+        title: string;
+        id: string;
+        image: ShopifyImage;
+      };
+    }[];
+  };
+  priceRange: VariantPrice;
+  compareAtPriceRange: VariantPrice;
+};
+
+export type ProductsData = {
+  products: {
+    nodes: ShopifyProductData[];
+  };
+};
+
+export type CategoryData = {
+  collection: {
+    handle: string;
+    products: {
+      edges: {
+        node: ShopifyProductData;
+      }[];
+    };
+  };
+};
