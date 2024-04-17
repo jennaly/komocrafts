33e44d9897274454b5c4ae7c1c5b32743848958c
jennaly <106183040+jennaly@users.noreply.github.com>
2024-04-17 15:08:56 -0700

    feat(ui): add scroll-triggered animations

--- src/app/ui/home/Featured.tsx
+++ src/app/ui/home/Featured.tsx
@@ -7,24 +7,60 @@ import { gilda } from "../fonts";
 import CTA from "../CTA";
 import ProductCard from "../product/ProductCard";
 
+const sectionVariants = {
+  offScreen: {
+    y: 50,
+    opacity: 0,
+  },
+  onScreen: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.1,
+      duration: 0.3,
+      staggerChildren: 0.5,
+      staggerDirection: 1,
+    },
+  },
+};
+
+const itemVariants = {
+  offScreen: {
+    opacity: 0,
+  },
+  onScreen: { opacity: 1 },
+};
+
 const Featured = ({ data }: { data: ProductsByCategoryData }) => {
   return (
-    <motion.section className="container mx-auto px-4 py-4 my-20 flex flex-col justify-around lg:justify-center gap-4">
-      <div className="flex w-full justify-between items-center lg:row-start-3">
+    <motion.section
+      initial="offScreen"
+      whileInView="onScreen"
+      viewport={{ once: true, amount: 0.3 }}
+      variants={sectionVariants}
+      className="container mx-auto px-4 py-4 my-20 flex flex-col justify-around lg:justify-center gap-4"
+    >
+      <motion.div
+        variants={itemVariants}
+        className="flex w-full justify-between items-center lg:row-start-3"
+      >
         <h3 className={`${gilda.className} text-2xl lg:text-3xl `}>
           Explore our newest arrivals
         </h3>
 
         <CTA link={"/all"}>Shop All</CTA>
-      </div>
+      </motion.div>
 
-      <div className="w-full flex overflow-x-scroll lg:overflow-hidden mt-10">
+      <motion.div
+        variants={itemVariants}
+        className="w-full flex overflow-x-scroll lg:overflow-hidden mt-10"
+      >
         <div className="flex flex-nowrap w-full gap-10 lg:gap-6 items-stretch">
           {data.collection.products.edges.map(({ node }) => (
             <ProductCard product={node} key={node.id} />
           ))}
         </div>
-      </div>
+      </motion.div>
     </motion.section>
   );
 };
