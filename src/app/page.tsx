8c60cdcea7065f5414d56b44d3d611b5e65a7053
jennaly <106183040+jennaly@users.noreply.github.com>
2024-04-18 15:36:06 -0700

    feat: categories collage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
-import { getProductsByCategory } from "./lib/data";
+import { getProductsByCategory, getSingleCategory } from "./lib/data";
+import Categories from "./ui/home/Categories";
 import Featured from "./ui/home/Featured";
 import Hero from "./ui/home/Hero";
+import HomepageCollage from "./ui/home/HomepageCollage";
+import Workshop from "./ui/home/Workshop";
 
 const Home = async () => {
   const featuredCollection = await getProductsByCategory("featured");
+  const coasters = await getSingleCategory("coasters");
+  const wallHangings = await getSingleCategory("wall-hangings");
+  const keychains = await getSingleCategory("keychains");
+  const supplies = await getSingleCategory("supplies");
 
+  console.log(coasters.collection.image.url);
   return (
-    <main className="">
+    <main className="bg-komo-bg">
       <Hero />
       <Featured data={featuredCollection} />
+      {/* <HomepageCollage /> */}
+      <Categories
+        coasters={coasters}
+        wallHangings={wallHangings}
+        keychains={keychains}
+        supplies={supplies}
+      />
+      <Workshop />
     </main>
   );
 };
---
+++ src/app/ui/home/Categories.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { gilda } from "../fonts";
+import { SingleCategoryData } from "@/app/lib/definitions";
+import clsx from "clsx";
+import Link from "next/link";
+
+const Categories = ({
+  coasters,
+  wallHangings,
+  keychains,
+  supplies,
+}: {
+  coasters: SingleCategoryData;
+  wallHangings: SingleCategoryData;
+  keychains: SingleCategoryData;
+  supplies: SingleCategoryData;
+}) => {
+  return (
+    <section className="h-screen grid grid-cols-12 grid-rows-12">
+      <Coasters data={coasters} />
+      <WallHangings data={wallHangings} />
+      <Keychains data={keychains} />
+      <Supplies data={supplies} />
+    </section>
+  );
+};
+
+const CategoryTitleStyle = `${gilda.className} text-3xl  col-span-full lg:my-auto`;
+const CategoryTextStyle =
+  "grid grid-cols-5 px-4 h-full bg-shop-overlay/50 hover:bg-shop-overlay/60 text-white";
+
+const Coasters = ({ data }: { data: SingleCategoryData }) => {
+  const { title, handle, image } = data.collection;
+  return (
+    <div
+      style={{ backgroundImage: `url('${image.url}')` }}
+      className={`w-full row-span-4 col-span-full lg:row-start-3 lg:col-span-6 lg:row-span-10 z-10 bg-bottom bg-cover`}
+    >
+      <Link href={`/shop/category/${handle}`}>
+        <div className={`${CategoryTextStyle} grid-rows-12`}>
+          <h4 className={`${CategoryTitleStyle} row-start-9 lg:row-start-11`}>
+            {title}
+          </h4>
+
+          <CategoryDetails className="row-start-11 lg:row-start-12 " />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+const WallHangings = ({ data }: { data: SingleCategoryData }) => {
+  const { title, handle, image } = data.collection;
+  return (
+    <div
+      style={{ backgroundImage: `url('${image.url}')` }}
+      className="w-full col-span-full row-span-4 lg:row-start-3 lg:col-span-6 lg:row-span-5 z-10 bg-cover bg-bottom bg-no-repeat"
+    >
+      <Link href={`/shop/category/${handle}`}>
+        <div className={`${CategoryTextStyle} grid-rows-6`}>
+          <h4 className={`${CategoryTitleStyle} row-start-5`}>{title}</h4>
+
+          <CategoryDetails className="row-start-6" />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+const Keychains = ({ data }: { data: SingleCategoryData }) => {
+  const { title, handle, image } = data.collection;
+  return (
+    <div
+      style={{ backgroundImage: `url('${image.url}')` }}
+      className="lg:row-start-8 lg:col-span-3 col-span-6 row-span-4 lg:row-span-5 z-10 bg-cover bg-center"
+    >
+      <Link href={`/shop/category/${handle}`}>
+        <div className={`${CategoryTextStyle} grid-rows-6`}>
+          <h4 className={`${CategoryTitleStyle} row-start-5`}>{title}</h4>
+
+          <CategoryDetails className="row-start-6" />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+const Supplies = ({ data }: { data: SingleCategoryData }) => {
+  const { title, handle, image } = data.collection;
+  return (
+    <div
+      style={{ backgroundImage: `url('${image.url}')` }}
+      className="lg:row-start-8 lg:col-span-3 col-span-6 row-span-4 lg:row-span-5 z-10 bg-cover bg-center"
+    >
+      <Link href={`/shop/category/${handle}`}>
+        <div className={`${CategoryTextStyle} grid-rows-6`}>
+          <h4 className={`${CategoryTitleStyle} row-start-5`}>{title}</h4>
+
+          <CategoryDetails className="row-start-6" />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+const CategoryDetails = ({ className }: { className: string }) => {
+  return (
+    <div
+      className={clsx(
+        "flex justify-between items-start col-span-12",
+        className
+      )}
+    >
+      <span className="hidden lg:inline-block text-sm">From $16.00</span>
+      <button className="underline underline-offset-4 lg:text-base text-sm">
+        See products
+      </button>
+    </div>
+  );
+};
+
+export default Categories;
