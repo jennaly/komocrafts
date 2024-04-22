224546c27fac63a30af3be239598f88c895b54d7
jennaly <106183040+jennaly@users.noreply.github.com>
2024-04-22 11:33:40 -0700

    feat(category page): create util function to format category page title

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -4,3 +4,9 @@ export const formatPrice = (price: string) =>
     currency: "CAD",
     minimumFractionDigits: 2,
   }).format(parseInt(price, 10));
+
+export const formatCategoryTitle = (title: string) =>
+  title
+    .split("")
+    .map((letter, index) => (index === 0 ? letter.toUpperCase() : letter))
+    .join("");
